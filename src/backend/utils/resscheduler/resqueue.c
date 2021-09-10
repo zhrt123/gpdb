a94d53730438c520f8e1072ef4a4ef8933053f09
Soumyadeep Chakraborty <soumyadeep2007@gmail.com>
2021-09-10 13:35:31 -0700

    Report duplicate portal ID error correctly
    Prior to this commit, a WARNING was issued upon encountering a duplicate
    portal in ResIncrementAdd(), instantly followed by an
    out-of-shared-memory ERROR in ResLockAcquire().
    
    A duplicate portal ID is a distinct error condition which is unrelated
    to a OOSM. So, introduce appropriate error types and messages to
    distinguish the two cases.
    
    (cherry picked from commit 86a9fb852c59d525dd80a06871895e711979c257)

--- src/backend/utils/resscheduler/resqueue.c
+++ src/backend/utils/resscheduler/resqueue.c
@@ -45,7 +45,10 @@
 
 static void ResCleanUpLock(LOCK *lock, PROCLOCK *proclock, uint32 hashcode, bool wakeupNeeded);
 
-static ResPortalIncrement *ResIncrementAdd(ResPortalIncrement *incSet, PROCLOCK *proclock, ResourceOwner owner);
+static ResPortalIncrement *ResIncrementAdd(ResPortalIncrement *incSet,
+										   PROCLOCK *proclock,
+										   ResourceOwner owner,
+										   ResIncrementAddStatus *status);
 static bool ResIncrementRemove(ResPortalTag *portaltag);
 
 static void ResWaitOnLock(LOCALLOCK *locallock, ResourceOwner owner, ResPortalIncrement *incrementSet);
@@ -135,6 +138,7 @@ ResLockAcquire(LOCKTAG *locktag, ResPortalIncrement *incrementSet)
 	ResourceOwner owner;
 	ResQueue	queue;
 	int			status;
+	ResIncrementAddStatus addStatus;
 
 	/* Setup the lock method bits. */
 	Assert(locktag->locktag_lockmethodid == RESOURCE_LOCKMETHOD);
@@ -339,17 +343,27 @@ ResLockAcquire(LOCKTAG *locktag, ResPortalIncrement *incrementSet)
 	 * Otherwise, we are going to take a lock, Add an increment to the
 	 * increment hash for this process.
 	 */
-	incrementSet = ResIncrementAdd(incrementSet, proclock, owner);
-	if (!incrementSet)
+	incrementSet = ResIncrementAdd(incrementSet, proclock, owner, &addStatus);
+	if (addStatus != RES_INCREMENT_ADD_OK)
 	{
+		/*
+		 * We have failed to add the increment. So decrement the requested
+		 * counters, relinquish locks and raise the appropriate error.
+		 */
 		lock->nRequested--;
 		lock->requested[lockmode]--;
 		LWLockRelease(ResQueueLock);
 		LWLockRelease(partitionLock);
-		ereport(ERROR,
-				(errcode(ERRCODE_OUT_OF_MEMORY),
-				 errmsg("out of shared memory adding portal increments"),
-				 errhint("You may need to increase max_resource_portals_per_transaction.")));
+		if (addStatus == RES_INCREMENT_ADD_OOSM)
+			ereport(ERROR,
+					(errcode(ERRCODE_OUT_OF_MEMORY),
+						errmsg("out of shared memory adding portal increments"),
+						errhint("You may need to increase max_resource_portals_per_transaction.")));
+		else
+			ereport(ERROR,
+					(errcode(ERRCODE_INTERNAL_ERROR),
+						errmsg("duplicate portal id %u for proc %d",
+							   incrementSet->portalId, incrementSet->pid)));
 	}
 
 	/*
@@ -1457,15 +1471,20 @@ ResPortalIncrementHashTableInit(void)
 /*
  * ResIncrementAdd -- Add a new increment element to the increment hash.
  *
- * Notes:
- *	Return a pointer to where the new increment is stored, or NULL if we
- *	are out of memory (or if we find a duplicate portalid).
+ * We return the increment added. We return NULL if we are run out of shared
+ * memory. In case there is an existing increment element in the hash table,
+ * we have encountered a duplicate portal - so we return the existing increment
+ * for ERROR reporting purposes. The status output argument is updated to
+ * indicate the outcome of the routine.
  *
  *	The resource queue lightweight lock (ResQueueLock) *must* be held for
  *	this operation.
  */
 static ResPortalIncrement *
-ResIncrementAdd(ResPortalIncrement *incSet, PROCLOCK *proclock, ResourceOwner owner)
+ResIncrementAdd(ResPortalIncrement *incSet,
+				PROCLOCK *proclock,
+				ResourceOwner owner,
+				ResIncrementAddStatus *status)
 {
 	ResPortalIncrement *incrementSet;
 	ResPortalTag portaltag;
@@ -1480,7 +1499,10 @@ ResIncrementAdd(ResPortalIncrement *incSet, PROCLOCK *proclock, ResourceOwner ow
 									   DDLNotSpecified,
 									   "",
 									   "") == FaultInjectorTypeSkip)
+	{
+		*status = RES_INCREMENT_ADD_OOSM;
 		return NULL;
+	}
 #endif
 
 	/* Set up the key. */
@@ -1494,6 +1516,7 @@ ResIncrementAdd(ResPortalIncrement *incSet, PROCLOCK *proclock, ResourceOwner ow
 
 	if (!incrementSet)
 	{
+		*status = RES_INCREMENT_ADD_OOSM;
 		return NULL;
 	}
 
@@ -1514,10 +1537,11 @@ ResIncrementAdd(ResPortalIncrement *incSet, PROCLOCK *proclock, ResourceOwner ow
 	{
 		/* We have added this portId before - something has gone wrong! */
 		ResIncrementRemove(&portaltag);
-		elog(WARNING, "duplicate portal id %u for proc %d", incSet->portalId, incSet->pid);
-		incrementSet = NULL;
+		*status = RES_INCREMENT_ADD_DUPLICATE_PORTAL;
+		return incrementSet;
 	}
 
+	*status = RES_INCREMENT_ADD_OK;
 	return incrementSet;
 }
 
