fc13ac07b59a33a80ff35d6a1d1dccf17ff69cea
Jimmy Yih <jyih@vmware.com>
2021-10-29 11:59:40 -0700

    Add CMockery test relating log_hostname GUC and AF_UNIX sockets
    There was a regression caused by a previous incomplete backport that
    made it so that invalid WARNING logs were being logged when
    log_hostname GUC was enabled and a user connects via AF_UNIX
    socket. As part of completing the backport to fix the issue, a change
    was made to getnameinfo_unix() to remove an unnecessary
    restriction. Add simple CMockery unit test to prevent future
    regressions.
    
    Example of WARNING log message:
    pg_getnameinfo_all() failed: Non-recoverable failure in name resolution
    
    Incomplete backport GPDB commit:
    https://github.com/greenplum-db/gpdb/commit/a9768e32a0a310bd7e7a45c089898542670bf2c2
    
    Related missing upstream Postgres patch that fixes the issue:
    https://github.com/postgres/postgres/commit/1997f34db4687e671690ed054c8f30bb501b1168

---
+++ src/backend/libpq/test/ip_test.c
@@ -0,0 +1,60 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include "cmockery.h"
+
+#include "../ip.c"
+
+test_pg_get_nameinfo_all_unix(bool log_hostname)
+{
+	int result = -1;
+	char remote_host[NI_MAXHOST] = {'\0'};
+	char remote_port[NI_MAXSERV] = {'\0'};
+	struct sockaddr_storage *addr = (struct sockaddr_storage *) malloc(sizeof(struct sockaddr_storage));
+	int salen = sizeof(struct sockaddr_storage);
+
+	/* initialize UNIX socket parameters */
+	addr->ss_family = AF_UNIX;
+#if defined(__darwin__)
+	strcpy(addr->__ss_pad1, "mock");
+#else
+	strcpy(addr->__ss_padding, "mock");
+#endif
+
+	/* This code is pretty much duplicated from postmaster.c:BackendInitialize() */
+	result = pg_getnameinfo_all(addr, salen,
+				    remote_host, sizeof(remote_host),
+				    remote_port, sizeof(remote_port),
+				    (log_hostname ? 0 : NI_NUMERICHOST) | NI_NUMERICSERV);
+
+	assert_true(result != EAI_FAIL);
+	assert_string_equal(remote_host, "[local]");
+	assert_string_equal(remote_port, "mock");
+}
+
+void
+test_pg_get_nameinfo_all_unix_log_hostname_false(void **state)
+{
+	test_pg_get_nameinfo_all_unix(false);
+}
+
+void
+test_pg_get_nameinfo_all_unix_log_hostname_true(void **state)
+{
+	test_pg_get_nameinfo_all_unix(true);
+}
+
+int
+main(int argc, char* argv[])
+{
+	cmockery_parse_arguments(argc, argv);
+
+	const UnitTest tests[] = {
+		unit_test(test_pg_get_nameinfo_all_unix_log_hostname_false),
+		unit_test(test_pg_get_nameinfo_all_unix_log_hostname_true)
+	};
+
+	MemoryContextInit();
+
+	return run_tests(tests);
+}
